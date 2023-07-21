860df1c318a4369c48f6ac242f141e7b611e88c5
Zewasik <shumahershik@gmail.com>
2023-07-21 19:02:01 +0300

    rewrite all code, create virtual dom

--- examples/index.js
+++ examples/index.js
@@ -1,34 +1,54 @@
-import Gachi from "/src/core/index.js"
+import Gachi from "../src/core/index.js"
 import { importCss } from "/src/modules/cssLoader.js"
 
 importCss("./index.css")
 
 const container = document.getElementById("root")
 
-// const elem = Gachi.createElement(
-// 	"div",
-// 	null,
-// 	Gachi.createElement("h1", { style: "display: flex;" }, "first title"),
-// 	Gachi.createElement(
-// 		"h2",
-// 		{
-// 			onClick: () => {
-// 				setA(a + 1)
-// 			},
-// 		},
-// 		"second title"
-// 	),
-// 	Gachi.createElement("div", { className: "header" }),
-// 	"aboba"
-// )
-
-const elem = (
-	<div>
-		<h1 style="display: flex">first title</h1>
-		<h2>second title</h2>
-		<div className="header"></div>
-		aboba
-	</div>
+const elem = Gachi.createElement(
+	"div",
+	null,
+	Gachi.createElement("h1", { style: "display: flex;" }, "first title"),
+	Gachi.createElement(
+		"div",
+		null,
+		Gachi.createElement("h3", { style: "display: flex;" }, "third title"),
+		"just div"
+	),
+	Gachi.createElement("div", { className: "header" }),
+	"aboba"
 )
 
+console.log(elem)
+// const elem = (
+// 	<div>
+// 		<h1 style="display: flex">first title</h1>
+// 		<h2>second title</h2>
+// 		<div className="header"></div>
+// 		aboba
+// 	</div>
+// )
+
 Gachi.render(elem, container)
+
+setTimeout(() => {
+	const elem = Gachi.createElement(
+		"div",
+		null,
+		Gachi.createElement("div", { className: "header" }),
+		Gachi.createElement("h2", { style: "display: flex;" }, "first title"),
+		Gachi.createElement(
+			"div",
+			null,
+			Gachi.createElement(
+				"h3",
+				{ style: "display: flex;" },
+				"third title"
+			),
+			"just div"
+		),
+		"aboba"
+	)
+
+	Gachi.render(elem, container)
+}, 3000)
